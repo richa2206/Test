969c6846a40df4fa4040a5ab4a57c902dd3f2086
richa2206 <58341416+richa2206@users.noreply.github.com>
2020-01-25 11:48:32 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,1 +1,57 @@
+//Book constructor
+function Book(title, author,isbn) {
+    this.title = title;
+    this.author = author;
+    this.isbn = isbn;
+}
 
+
+
+//UI Constructor
+function UI() {
+
+}
+
+//Add Book to list
+
+UI.prototype.addBookToList = function(book){
+    const list = document.getElementById('book-list');
+    //Create tr element
+    const row = document.createElement('tr');
+     //Insert cols
+     row.innerHTML = `
+     <td>${book.title}</td>
+     <td>${book.author}</td>
+     <td>${book.isbn}</td>
+     <td><a href ="#" class="delete">X<a></td>
+     `;
+
+
+list.appendChild(row);
+}
+
+
+//Event Listener
+document.getElementById('book-form').addEventListener('submit', 
+function(e){
+    //Get form values
+    const title = document.getElementById('title').value,
+           author = document.getElementById('author').value,
+           isbn = document.getElementById('isbn').value
+// Instantiate book
+ const book = new Book(title, author, isbn);
+
+
+//Instantiate UI
+const ui = new UI();
+
+
+
+//Add book to list
+ui.addBookToList(Book);
+
+
+           e.preventDefault();
+
+
+});
